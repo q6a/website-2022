5a5013243afac50a9aa58b0c637a8bf5acaabad3
Yasin Junet <hello@yasinjunet.com>
2022-12-19 20:31:25 +0700

    style: add spacing into hero section

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -5,34 +5,36 @@ import { H1, BodyText } from "./Typography";
 
 const HomeHero = () => {
   return (
-    <div className="section bg-hero text-white">
-      <div className="container">
-        <div className="row">
-          <div className="col-12 col-lg-6 d-flex flex-column justify-content-center gap-3 text-center">
-            <H1>
-              Your partner in <span>video marketing</span>
-            </H1>
-            <BodyText>
-              Transcribe, translate and dub videos in few sample steps
-              <br />
-              Try it now. No account required!
-            </BodyText>
-            <div className="d-flex justify-content-center gap-3">
-              <Link className="btn btn-light text-primary" to="/">
-                Login
-              </Link>
-              <Link className="btn btn-primary" to="/">
-                Sign up
-              </Link>
+    <div className="bg-brand-light pt-3">
+      <div className="section-sm bg-hero text-white rounded-2 mx-3">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-lg-6 d-flex flex-column justify-content-center gap-3 text-center">
+              <H1>
+                Your partner in <span>video marketing</span>
+              </H1>
+              <BodyText>
+                Transcribe, translate and dub videos in few sample steps
+                <br />
+                Try it now. No account required!
+              </BodyText>
+              <div className="d-flex justify-content-center gap-3">
+                <Link className="btn btn-light text-primary" to="/">
+                  Login
+                </Link>
+                <Link className="btn btn-primary" to="/">
+                  Sign up
+                </Link>
+              </div>
             </div>
-          </div>
-          <div className="col-12 col-lg-6 bg-focus-hero d-flex align-items-center justify-content-center">
-            <img
-              src={withPrefix("images/img-hero.png")}
-              alt="Play video"
-              className="img-hero"
-              loading="lazy"
-            />
+            <div className="col-12 col-lg-6 bg-focus-hero d-flex align-items-center justify-content-center">
+              <img
+                src={withPrefix("images/img-hero.png")}
+                alt="Play video"
+                className="img-hero"
+                loading="lazy"
+              />
+            </div>
           </div>
         </div>
       </div>
