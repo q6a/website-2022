770bbeaca2b3ee77a62a3f7e23b1aa29827556df
Yasin Junet <hello@yasinjunet.com>
2023-04-07 15:15:36 +0700

    fix: adjust hero section on home page

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -12,29 +12,22 @@ const HomeHero = () => {
       <div className="section-sm bg-hero text-white rounded-2 mx-3">
         <div className="container">
           <div className="row">
-            <div className="col-12 col-lg-6 d-flex flex-column justify-content-center gap-3">
-              <H1>
-                <Trans i18nKey="heroTitle" components={[<span />]} />
-              </H1>
-              <BodyText>
-                <Trans i18nKey="heroDescription" components={[<br />]} />
-              </BodyText>
-              {/* <div className="d-flex gap-3">
-                <Link className="btn btn-light text-primary" to="/">
-                  {t("login")}
-                </Link>
-                <Link className="btn btn-primary" to="/">
-                  {t("signUp")}
-                </Link>
-              </div> */}
-            </div>
-            <div className="col-12 col-lg-6 bg-focus-hero d-flex align-items-center justify-content-center">
-              <img
+            <div className="col-12 bg-focus-hero d-flex flex-column align-items-center justify-content-center gap-5">
+              {/* <img
                 src={withPrefix("images/img-hero.png")}
                 alt="Play video"
                 className="img-hero"
                 loading="lazy"
-              />
+              /> */}
+              <div className="hero-title fs-1 fw-bold">
+                <Trans i18nKey="heroTitle" components={[<span />]} />
+              </div>
+              <BodyText>
+                <Trans i18nKey="heroDescription" components={[<br />]} />
+              </BodyText>
+              <button type="button" className="btn btn-primary" disabled>
+                Coming soon!
+              </button>
             </div>
           </div>
         </div>
