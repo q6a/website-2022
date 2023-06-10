b7119488d1cf2bfa1a26c35f8e7125c9dd4d3ddc
Yasin Junet <hello@yasinjunet.com>
2023-06-10 17:08:05 +0700

    feat: partner-client logos autoscroll

--- src/components/HomeClients.tsx
+++ src/components/HomeClients.tsx
@@ -2,6 +2,8 @@
 import React from "react";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import { useTranslation } from "gatsby-plugin-react-i18next";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import { AutoScroll } from "@splidejs/splide-extension-auto-scroll";
 
 import { H2 } from "./Typography";
 
@@ -16,19 +18,41 @@ const HomeClients = ({ partners, clients }: any) => {
             <H2 classes="text-gray" hasSeparator>
               {t("clientSectionTitle")}
             </H2>
-            <div className="row mt-5">
-              {clients.map(({ id, localFile }: any) => {
-                const image = getImage(localFile);
-                return (
-                  <div key={id} className="col-6 col-md-4 px-5">
-                    <GatsbyImage
-                      image={image}
-                      alt={localFile?.name}
-                      className="px-4 py-2 logo-item"
-                    />
-                  </div>
-                );
-              })}
+            <div className="row mt-5 logo-marquee">
+              <Splide
+                options={{
+                  perPage: 3,
+                  arrows: false,
+                  pagination: false,
+                  autoScroll: {
+                    speed: 1,
+                    pauseOnHover: false,
+                    pauseOnFocus: false,
+                    rewind: true,
+                  },
+                }}
+                extensions={{ AutoScroll }}
+                aria-label="Clients"
+              >
+                {clients.map(({ id, localFile }: any) => {
+                  const image = getImage(localFile);
+                  return (
+                    <SplideSlide key={`client-${id}`}>
+                      <div className="px-5 h-100 d-flex align-items-center">
+                        <GatsbyImage
+                          image={image}
+                          alt={localFile?.name}
+                          className="px-4 py-2 logo-item"
+                          height={532}
+                          width="auto"
+                          maxWidth="100%"
+                          objectFit="contain"
+                        />
+                      </div>
+                    </SplideSlide>
+                  );
+                })}
+              </Splide>
             </div>
           </div>
         </div>
@@ -39,19 +63,37 @@ const HomeClients = ({ partners, clients }: any) => {
             <H2 classes="text-gray" hasSeparator>
               {t("partnerSectionTitle")}
             </H2>
-            <div className="row mt-5">
-              {partners.map(({ id, localFile }: any) => {
-                const image = getImage(localFile);
-                return (
-                  <div key={id} className="col-6 col-md-4 px-5">
-                    <GatsbyImage
-                      image={image}
-                      alt={localFile?.name}
-                      className="px-4 py-2 logo-item"
-                    />
-                  </div>
-                );
-              })}
+            <div className="row mt-5 logo-marquee">
+              <Splide
+                options={{
+                  perPage: 3,
+                  arrows: false,
+                  pagination: false,
+                  autoScroll: {
+                    speed: -1,
+                    pauseOnHover: false,
+                    pauseOnFocus: false,
+                    rewind: true,
+                  },
+                }}
+                extensions={{ AutoScroll }}
+                aria-label="Clients"
+              >
+                {partners.map(({ id, localFile }: any) => {
+                  const image = getImage(localFile);
+                  return (
+                    <SplideSlide key={`partner-${id}`}>
+                      <div className="px-5 h-100 d-flex align-items-center">
+                        <GatsbyImage
+                          image={image}
+                          alt={localFile?.name}
+                          className="px-4 py-2 logo-item"
+                        />
+                      </div>
+                    </SplideSlide>
+                  );
+                })}
+              </Splide>
             </div>
           </div>
         </div>
