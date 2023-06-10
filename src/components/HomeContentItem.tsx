3e565eb4ff63866ccdf2df18d17da0e230ca9ad6
Yasin Junet <hello@yasinjunet.com>
2023-06-10 14:17:05 +0700

    fix: case studies clickable area including image, title, and description

--- src/components/HomeContentItem.tsx
+++ src/components/HomeContentItem.tsx
@@ -27,39 +27,39 @@ const HomeContentItem = ({
 }: HomeContentItemProps) => {
   const image = getImage(img?.localFile);
   return (
-    <div className="mx-0 mx-md-5 mt-3">
-      <div className="content-item rounded-2 text-white mx-4">
-        <div className="row h-100">
-          <div className="col-12 col-lg-6 d-flex">
-            <div className="px-4 py-4 w-100">
-              <div className="rounded-2 overflow-hidden">
-                {/* @ts-ignore */}
-                <GatsbyImage image={image} alt={imgAlt} />
+    <Link
+      to={link}
+      className="content-link text-white text-decoration-none fw-semibold"
+    >
+      <div className="mx-0 mx-md-5 mt-3">
+        <div className="content-item rounded-2 text-white mx-4">
+          <div className="row h-100">
+            <div className="col-12 col-lg-6 d-flex">
+              <div className="px-4 py-4 w-100">
+                <div className="rounded-2 overflow-hidden">
+                  {/* @ts-ignore */}
+                  <GatsbyImage image={image} alt={imgAlt} />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-12 col-lg-6 text-end d-flex flex-column align-items-end justify-content-around gap-5 p-5 h-100">
-            <div className="w-75 text-right">
-              <H1>{title}</H1>
-            </div>
-            <BodyContentText>{description}</BodyContentText>
-            <Link
-              to={link}
-              className="content-link text-white text-decoration-none fw-semibold"
-            >
+            <div className="col-12 col-lg-6 text-end d-flex flex-column align-items-end justify-content-around gap-5 p-5 h-100">
+              <div className="w-75 text-right">
+                <H1>{title}</H1>
+              </div>
+              <BodyContentText>{description}</BodyContentText>
               <span>Read the story</span>
-            </Link>
-            {/* <BodyContentText classes="mb-0">
+              {/* <BodyContentText classes="mb-0">
               {authorName}
               <br />
               {authorRoles},
               <br />
               {authorOrg}
             </BodyContentText> */}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
