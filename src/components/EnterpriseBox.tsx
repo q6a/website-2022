b8e8f5609f7f57a536f557d03d0c4ddcca47892c
Yasin Junet <hello@yasinjunet.com>
2023-07-02 07:10:10 +0700

    fix: replace enterprise icons with images

--- src/components/EnterpriseBox.tsx
+++ src/components/EnterpriseBox.tsx
@@ -2,18 +2,13 @@ import React from "react";
 import { withPrefix } from "gatsby";
 import { Link } from "gatsby-plugin-react-i18next";
 
-const Card = ({ title, desc, icon }: any) => (
-  <div className="enterprise-box rounded-2 shadow text-center p-5 d-flex flex-column align-items-center gap-4">
-    <img
-      src={withPrefix(icon)}
-      alt={title}
-      width={80}
-      height={80}
-      loading="lazy"
-    />
-    <span className="fs-5 fw-bold">{title}</span>
-    <p className="mb-0">{desc}</p>
-  </div>
+const Card = ({ icon }: any) => (
+  <div
+    className="enterprise-box rounded-2 shadow text-center p-5 d-flex flex-column align-items-center gap-4"
+    style={{
+      backgroundImage: `url(${withPrefix(icon)})`,
+    }}
+  />
 );
 
 const EnterpriseBox = (item: any) => {
