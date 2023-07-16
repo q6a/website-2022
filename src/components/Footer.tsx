e99bdd7ef720dd6bcf1f4743445650a6e7126695
Yasin Junet <hello@yasinjunet.com>
2023-07-16 15:19:58 +0700

    feat: show subscribe newsletter to hero section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { withPrefix } from "gatsby";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -8,9 +8,9 @@ import {
   faYoutube,
   faInstagram,
 } from "@fortawesome/free-brands-svg-icons";
-import { useI18next, useTranslation } from "gatsby-plugin-react-i18next";
-import * as z from "zod";
+import { useTranslation } from "gatsby-plugin-react-i18next";
 
+import Newsletter from "./Newsletter";
 import WrapperLink from "./WrapperLink";
 import useFooterCompanyNavigation from "../hooks/useFooterCompanyNav";
 import useFooterProductNavigation from "../hooks/useFooterProductNav";
@@ -18,15 +18,7 @@ import useFooterResourcesNavigation from "../hooks/useFooterResourcesNav";
 import useFooterEnterpriseNavigation from "../hooks/useFooterEnterpriseNav";
 import useSocialLinks from "../hooks/useSocialLinks";
 
-const emailSchema = z
-  .object({
-    email: z.string().email(),
-    locale: z.enum(["en", "id"]),
-  })
-  .required();
-
 const Footer = () => {
-  const { language } = useI18next();
   const currentYear = new Date().getFullYear();
   const companyNavigation = useFooterCompanyNavigation();
   const productNavigation = useFooterProductNavigation();
@@ -34,47 +26,6 @@ const Footer = () => {
   const enterpriseNavigation = useFooterEnterpriseNavigation();
   const socialLinks = useSocialLinks();
   const { t } = useTranslation();
-  const [inputEmail, setInputEmail] = useState("");
-  const [isError, setIsError] = useState(false);
-  const [isSuccess, setIsSuccess] = useState(false);
-
-  const subscribeNewUser = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    try {
-      const newsletterParams = {
-        email: inputEmail,
-        locale: language,
-      };
-
-      const paramsValidation = emailSchema.safeParse(newsletterParams);
-
-      if (!paramsValidation.success) {
-        setIsError(true);
-      } else {
-        fetch(
-          `${withPrefix("/api/newsletter/subscribe")}?${new URLSearchParams({
-            email: inputEmail,
-            locale: language,
-          })}`
-        )
-          .then((response) => {
-            if (!response.ok) {
-              throw new Error("Failed to subsribe");
-            }
-            return response.json();
-          })
-          .then((response) => {
-            if (response.status === 200) {
-              setInputEmail("");
-              setIsSuccess(true);
-            }
-          });
-      }
-    } catch (err) {
-      setIsError(true);
-    }
-  };
 
   return (
     <>
@@ -93,38 +44,7 @@ const Footer = () => {
                 <span className="fs-14 fw-semibold">
                   {t("subscribeNewsletter")}
                 </span>
-                <form onSubmit={(e: React.FormEvent) => subscribeNewUser(e)}>
-                  <div className="input-group">
-                    <input
-                      type="email"
-                      className="form-control"
-                      placeholder={t("inputNewsletterPlaceholder") || ""}
-                      aria-label={t("inputNewsletterPlaceholder") || ""}
-                      aria-describedby="button-subscribe"
-                      value={inputEmail}
-                      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                        setInputEmail(e.target.value)
-                      }
-                    />
-                    <button
-                      className="btn btn-gradient"
-                      type="submit"
-                      id="button-subscribe"
-                    >
-                      {t("subscribe")}
-                    </button>
-                  </div>
-                </form>
-                {isSuccess && (
-                  <span className="fs-14 fw-semibold text-success mb-3">
-                    Thank you for subscribing!
-                  </span>
-                )}
-                {isError && (
-                  <span className="fs-14 fw-semibold text-danger mb-3">
-                    Failed to subscribing
-                  </span>
-                )}
+                <Newsletter />
               </div>
             </div>
             <div className="col-12 col-lg-2 ps-3 ps-lg-5 mt-5 mt-lg-0">
